10544db3c59d5585e70d5af7e5cf229da1017784
Jan Sviland <jan.sviland@systek.no>
2023-05-22 13:12:54 +0200

    testing with time

--- 1.UnitTesting/2.DeepDive/src/EdgeCases/Greeter.cs
+++ 1.UnitTesting/2.DeepDive/src/EdgeCases/Greeter.cs
@@ -1,10 +1,23 @@
 ﻿namespace EdgeCases;
 
+public interface IClock
+{
+    DateTime Now { get; }
+}
+
 public class Greeter
 {
+    private readonly IClock _clock;
+
+    public Greeter(IClock clock)
+    {
+        _clock = clock;
+    }
+
     public string GenerateGreetText()
     {
-        var dateTimeNow = DateTime.Now;
+        // use timeprovider, dotnet 8
+        var dateTimeNow = _clock.Now;
         return dateTimeNow.Hour switch
         {
             >= 5 and < 12 => "Good morning",
--- 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/Services/QuoteServiceTests.cs
+++ 1.UnitTesting/2.DeepDive/tests/ForeignExchange.Api.Tests.Unit/Services/QuoteServiceTests.cs
@@ -3,8 +3,8 @@ using ForeignExchange.Api.Repositories;
 using ForeignExchange.Api.Services;
 using ForeignExchange.Api.Validation;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using Xunit;
 
 namespace ForeignExchange.Api.Tests.Unit.Services;
@@ -24,9 +24,9 @@ public class QuoteServiceTests
     public async Task GetQuoteAsync_ShouldReturnQuote()
     {
         // Arrange
-        var fromCurrency = "EUR";
-        var toCurrency = "USD";
-        var amount = 100m;
+        const string fromCurrency = "EUR";
+        const string toCurrency = "USD";
+        const decimal amount = 100m;
 
         var expected = new ConversionQuote
         {
@@ -51,29 +51,65 @@ public class QuoteServiceTests
         Assert.Equivalent(expected, actual);
     }
 
+    [Theory]
+    [InlineData("EUR", "USD")]
+    [InlineData("USD", "EUR")]
+    [InlineData("EUR", "GBP")]
+    public async Task GetQuoteAsync_ShouldReturnQuote_WhenCurrenciesAreValid(string fromCurrency, string toCurrency)
+    {
+        // Arrange
+        // const string fromCurrency = "EUR";
+        // const string toCurrency = "USD";
+        const decimal amount = 100m;
+
+        var expected = new ConversionQuote
+        {
+            BaseCurrency = fromCurrency,
+            QuoteCurrency = toCurrency,
+            BaseAmount = amount,
+            QuoteAmount = 120m
+        };
+
+        _ratesRepository
+            .GetRateAsync(fromCurrency, toCurrency)
+            .Returns(new FxRate()
+            {
+                Rate = 1.2m
+            });
+
+        // Act
+        var actual = await _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
+
+        // Assert
+        Assert.Equivalent(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetQuoteAsync_ShouldReturnNull_WhenNoRateExists()
+    {
+        // Arrange
+        const string fromCurrency = "ABC";
+        const string toCurrency = "DEF";
+        const decimal amount = 100m;
+
+        _ratesRepository
+            .GetRateAsync(fromCurrency, toCurrency)
+            .ReturnsNull();
+
+        // Act
+        var actual = await _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);
+
+        // Assert
+        Assert.Null(actual);
+    }
+
     [Fact]
     public async Task GetQuoteAsync_ShouldThrowException_WhenNegative()
     {
         // Arrange
-        var fromCurrency = "EUR";
-        var toCurrency = "USD";
-        var amount = -100m;
-
-        // var expected = new ConversionQuote
-        // {
-        //     BaseCurrency = fromCurrency,
-        //     QuoteCurrency = toCurrency,
-        //     BaseAmount = amount,
-        //     QuoteAmount = 120m
-        // };
-
-        // _ratesRepository
-        //     .GetRateAsync(fromCurrency, toCurrency)
-        //     .Returns(new FxRate()
-        //     {
-        //         Rate = 1.2m
-        //     });
-
+        const string fromCurrency = "EUR";
+        const string toCurrency = "USD";
+        const decimal amount = -100m;
 
         // Act
         var action = () => _sut.GetQuoteAsync(fromCurrency, toCurrency, amount);

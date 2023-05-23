6d23bfe285ff751a5d20e4860806f60f5f9e9387
Nick Chapsas <nick.chapsas@gmail.com>
2023-05-23 13:47:32 +0200

    pre ex 2

---
+++ 2.IntegrationTesting/1.ApiTesting/src/GitHub.Api.Fake/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+var settings = new WireMockServerSettings
+{
+    Urls = new[] { "https://localhost:9095/" },
+    StartAdminInterface = true,
+    ProxyAndRecordSettings = new ProxyAndRecordSettings
+    {
+        Url = "https://api.github.com",
+        SaveMapping = true,
+        SaveMappingToFile = true,
+        SaveMappingForStatusCodePattern = "2xx"
+    }
+};
+
+var wiremockServer = WireMockServer.Start(settings);
+
+Console.WriteLine($"Server started at: {wiremockServer.Url}");
+
+Console.ReadKey();
+
+wiremockServer.Dispose();
---
+++ 2.IntegrationTesting/1.ApiTesting/tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
@@ -0,0 +1,73 @@
+using System.Data.Common;
+using Customers.Api.Database;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Net.Http.Headers;
+using Npgsql;
+using Respawn;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace Customers.Api.Tests.Integration;
+
+public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+{
+    private DbConnection _dbConnection = null!;
+    private Respawner _respawner = null!;
+
+    public GitHubApiServer GitHubApiServer { get; } = new();
+
+    private readonly PostgreSqlContainer _dbContainer =
+        new PostgreSqlBuilder()
+            .WithDatabase("mydb")
+            .WithUsername("workshop")
+            .WithPassword("changeme")
+            .Build();
+
+    public HttpClient Client { get; private set; } = null!;
+    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(IDbConnectionFactory));
+            services.AddSingleton<IDbConnectionFactory>(_ =>
+                new NpgsqlConnectionFactory(_dbContainer.GetConnectionString()));
+            
+            services.AddHttpClient("GitHub", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(GitHubApiServer.Url);
+                httpClient.DefaultRequestHeaders.Add(
+                    HeaderNames.Accept, "application/vnd.github.v3+json");
+                httpClient.DefaultRequestHeaders.Add(
+                    HeaderNames.UserAgent, $"Workshop-{Environment.MachineName}");
+            });
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        GitHubApiServer.Start();
+        await _dbContainer.StartAsync();
+        Client = CreateClient();
+        _dbConnection = new NpgsqlConnection(_dbContainer.GetConnectionString());
+        await _dbConnection.OpenAsync();
+        _respawner = await Respawner.CreateAsync(_dbConnection, new RespawnerOptions
+        {
+            DbAdapter = DbAdapter.Postgres,
+            SchemasToInclude = new []{ "public" }
+        });
+    }
+
+    public async Task ResetDatabaseAsync()
+    {
+        await _respawner.ResetAsync(_dbConnection);
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        GitHubApiServer.Dispose();
+    }
+}
